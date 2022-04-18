6c222f2b4ec57fd3c9db50a7497faa60b41cb87d
Marcelo Camargo <camargo.advanced@gmail.com>
2022-04-18 19:49:47 -0300

    test full screen

--- game.py
+++ game.py
@@ -19,6 +19,7 @@ class Game():
             depth=COLOR_DEPTH)  # initialize the display
         self.screen_rect = self.screen.get_rect()
         pygame.display.set_caption("Vegan Bunny POS")  # set window caption
+        pygame.mouse.set_visible(False)
         self.clock = pygame.time.Clock()  # the time starts
 
     def new_game(self):
